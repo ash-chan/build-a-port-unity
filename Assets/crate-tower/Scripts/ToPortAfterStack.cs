6e2d9b0d08ea9353abcd5fef278a8195dfb4c2a0
Ashley Chan <ashl0005@e.ntu.edu.sg>
2019-10-13 14:23:50 +0800

    crate tower game

---
+++ Assets/crate-tower/Scripts/ToPortAfterStack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToPortAfterStack : MonoBehaviour {
+
+
+
+    public void OnPlayClick(string MainPage)
+    {
+		Money.currentMoney += TheStack.actualScore * 15;
+        Debug.Log("Play button has been clicked!");
+        Application.LoadLevel(MainPage);
+    }
+}
---
+++ Assets/crate-tower/Scripts/toStackGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class toStackGame : MonoBehaviour {
+
+    public GameObject instruction;
+
+    public void OnMouseDown()
+    {
+        //instruction.SetActive(false);
+    }
+}
